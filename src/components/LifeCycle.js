7c5f731f4b18f0ab8f55faaab5142cda7d10f3d8
이지훈 <ljh@MacBook-Pro.local>
2018-02-14 22:45:19 +0900

    components test

--- src/components/LifeCycle.js
+++ src/components/LifeCycle.js
@@ -9,6 +9,14 @@ class LifeCycle extends React.Component {
     this.state = {
       hello: '난 state'
     };
+
+    this.Change = this.Change.bind(this);
+  }
+
+  Change() {
+    this.setState({
+      hello: 'changed'
+    });
   }
 
   componentWillMount() {
@@ -22,10 +30,52 @@ class LifeCycle extends React.Component {
       'componentDidMount 컴포넌트 마운트 된 후 === 컴포넌트 생성 완료'
     );
   }
+
+  // 컴포넌트가 새로운 props를 받았을 때
+  componentWillReceiveProps(nextProps) {
+    console.log('componentWillReceiveProps: ' + JSON.stringify(nextProps));
+  }
+
+  // 컴포넌트를 표시할지 안할지 결정하는 부분
+  shouldComponentUpdate(nextProps, nextState) {
+    console.log(
+      'shouldComponentUpdate: ' +
+        JSON.stringify(nextProps) +
+        ' ' +
+        JSON.stringify(nextState)
+    );
+    return true;
+  }
+
+  // 컴포넌트 업데이트 되기 전
+  componentWillUpdate(nextProps, nextState) {
+    console.log(
+      'componentWillUpdate: ' +
+        JSON.stringify(nextProps) +
+        ' ' +
+        JSON.stringify(nextState)
+    );
+  }
+
+  // 컴포넌트의 업데이트가 끝났을 때
+  componentDidUpdate(prevProps, prevState) {
+    console.log(
+      'componentDidUpdate: ' +
+        JSON.stringify(prevProps) +
+        ' ' +
+        JSON.stringify(prevState)
+    );
+  }
+
+  // 컴포넌트가 DOM 에서 제거되었을 때
+  componentWillUnmount() {
+    console.log('componentWillUnmount');
+  }
   render() {
     return (
       <div>
         <h3>{this.state.hello}</h3>
+        <button onClick={this.Change}>Change</button>
       </div>
     );
   }
